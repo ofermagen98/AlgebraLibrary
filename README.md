536d05578deff7b41eaa00c5da0591284f0d5bdc
ofermagen98 <ofermagen98@gmail.com>
2019-04-10 15:46:06 +0300

    basic RSA implementation - main.cpp

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Bleichenbacher Attack
 
 * This branch will implement the bleichenbacher-attack
-* currently the attack is implemented without the LLL modification
+* Currently the attack is implemented without the LLL modification
 * We've chose to use Crypto++ for the implementation and demonstration since it is a 
--- main.cpp
+++ main.cpp
@@ -1,47 +1,38 @@
+#include <cryptopp/cryptlib.h>
 #include <cryptopp/integer.h>
 #include <cryptopp/modarith.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/rng.h>
 #include <cryptopp/rsa.h>
 using namespace CryptoPP;
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
 class Server
 {
-private:
-	RSA::PrivateKey privateKey;
-public:
-	RSA::PublicKey publicKey;
+    private:
+    RSA::PrivateKey privateKey;
 
-	Server(RandomNumberGenerator &rng){
-		privateKey.GenerateRandomWithKeySize(rng, 3072);
-		publicKey = RSA::PublicKey(privateKey);
-	}
-	~Server(){
+    public:
+    int k = 384;
+    RSA::PublicKey publicKey;
 
-	}
-
-	bool is
-	
+    Server(RandomNumberGenerator& rng)
+    {
+        privateKey.GenerateRandomWithKeySize(rng, k * 8);
+        publicKey = RSA::PublicKey(privateKey);
+    }
+    ~Server()
+    {
+    }
 };
-
-
 
 
 int main(int argc, char* argv[])
 {
-  Integer m("4294967295"), n("0x1000000000000000000000000000000"), j;
-  j = 1999;
-
-  ModularArithmetic ma(j);
-
-  cout << "n+m mod j: " << ma.Add(n, m) << endl;
-  cout << "n-m mod j: " << ma.Subtract(n, m) << endl;
-  cout << "n*m mod j: " << ma.Multiply(n, m) << endl;
-  cout << "n/m mod j: " << ma.Divide(n, m) << endl;
-  cout << "n%m mod j: " << ma.Reduce(n, m) << endl;
-  cout << "n^m mod j: " << ma.Exponentiate(n, m) << endl;
-
-  return 0;
+    AutoSeededRandomPool prng;
+    Server srv(prng);
+    return 0;
 }
