43a3052401e22c4464d783e746dfe367ce1eb32b
ofermagen98 <ofermagen98@gmail.com>
2019-04-10 15:23:33 +0300

    basic files

---
+++ README.md
@@ -0,0 +1,3 @@
+This branch will implement the blachenbacher-attack
+currently the attack is implemented without the LLL modification
+We've chose to use Crypto++ for the implementation and demonstration since it is a 
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <cryptopp/integer.h>
+#include <cryptopp/modarith.h>
+#include <cryptopp/rsa.h>
+using namespace CryptoPP;
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Server
+{
+private:
+	RSA::PrivateKey privateKey;
+public:
+	RSA::PublicKey publicKey;
+
+	Server(RandomNumberGenerator &rng){
+		privateKey.GenerateRandomWithKeySize(rng, 3072);
+		publicKey = RSA::PublicKey(privateKey);
+	}
+	~Server(){
+
+	}
+
+	bool 
+	
+};
+
+
+
+
+int main(int argc, char* argv[])
+{
+  Integer m("4294967295"), n("0x1000000000000000000000000000000"), j;
+  j = 1999;
+
+  ModularArithmetic ma(j);
+
+  cout << "n+m mod j: " << ma.Add(n, m) << endl;
+  cout << "n-m mod j: " << ma.Subtract(n, m) << endl;
+  cout << "n*m mod j: " << ma.Multiply(n, m) << endl;
+  cout << "n/m mod j: " << ma.Divide(n, m) << endl;
+  cout << "n%m mod j: " << ma.Reduce(n, m) << endl;
+  cout << "n^m mod j: " << ma.Exponentiate(n, m) << endl;
+
+  return 0;
+}
