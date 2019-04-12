8862195f16aa3d37b70dafccc50730dbf36e7549
ofermagen98 <ofermagen98@gmail.com>
2019-04-12 18:16:13 +0300

    first debbuged version, moved encoding to server

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include <exception>
 #include <iomanip>
 #include <iostream>
-#include <set>
 #include <sstream>
 #include <unordered_map>
 #include <vector>
@@ -26,15 +25,16 @@ uint16_t hex2int(char c)
 
 class Server
 {
-    private:
+    public:
     CryptoPP::RSA::PrivateKey privateKey;
     CryptoPP::AutoSeededRandomPool prng;
 
     public:
+    const int keysize;
     CryptoPP::RSA::PublicKey publicKey;
     std::string PS;
 
-    Server(int keysize)
+    Server(int keysize) : keysize(keysize)
     {
         privateKey.GenerateRandomWithKeySize(prng, keysize);
         publicKey = CryptoPP::RSA::PublicKey(privateKey);
@@ -43,76 +43,65 @@ class Server
     bool isPkcsConforming(const Integer& c)
     {
         Integer m = privateKey.CalculateInverse(prng, c);
+        if (m.BitCount() != keysize - 14) return false;
+        int sz = m.ByteCount();
 
-        std::stringstream _s;
-        _s << std::hex << m;
-        std::string s = _s.str();
-        s.pop_back();
-
-        if (s.length() < 21 || s.length() % 2 == 0) return false;
-        if (s[0] != '2') return false;
-        for (int i = 1; i < 2 * 8 + 1; i += 2)
-            if (s[i] == '0' && s[i + 1] == '0') return false;
-        for (int i = 2 * 8 + 1; i < s.length(); i += 2)
-            if (s[i] == '0' && s[i + 1] == '0') return true;
+        if (m.GetByte(sz - 1) != 2) return false;
+        for (int i = 0; i < 8; ++i)
+            if (m.GetByte(sz - 2 - i) == 0) return false;
+        for (int i = sz - 10; i >= 0; --i)
+            if (m.GetByte(i) == 0) return true;
         return false;
+    }
+
+
+    Integer PkcsEncrypt(const std::string& s)
+    {
+        int max_size = publicKey.GetModulus().ByteCount() - 11;
+        if (s.length() > max_size)
+            throw std::overflow_error("message too long, max size is " + std::to_string(max_size));
+
+        int pad = publicKey.GetModulus().ByteCount() - 3 - s.length();
+        uint16_t rnd;
+        Integer res = Integer::Power2(keysize) - 1;
+        int sz = res.ByteCount();
+        res.SetByte(sz - 2, 2);
+
+        for (int i = 0; i < pad; ++i)
+        {
+            do
+            {
+                rnd = random() % 256;
+            } while (rnd == 0);
+            res.SetByte(sz - 3 - i, rnd);
+        }
+        res.SetByte(sz - 3 - pad, 0);
+
+        for (int i = 0; i < s.length(); ++i)
+            res.SetByte((sz - 4 - pad) - i, s[i]);
+
+        res.SetByte(sz - 1, 0);
+
+        return publicKey.ApplyFunction(res);
     }
 };
 
-Integer PkcsEncrypt(Server& srv, const std::string& s)
+std::string PkcsDecode(const Integer& m, int keysize)
 {
-    int max_size = srv.publicKey.GetModulus().ByteCount() - 11;
-    if (s.length() > max_size)
-        throw std::overflow_error("message too long, max size is " + std::to_string(max_size));
-
-    int pad = srv.publicKey.GetModulus().ByteCount() - 3 - s.length();
-    uint16_t rnd;
-
-    std::stringstream ss;
-    ss << "0x0002";
-    for (int i = 0; i < pad; ++i)
-    {
-        do
-        {
-            rnd = random() % 256;
-        } while (rnd == 0);
-        if (rnd < 16) ss << "0";
-        ss << std::hex << rnd;
-    }
-    ss << "00";
-
-    for (uint16_t c : s)
-    {
-        if (c < 16) ss << "0";
-        ss << std::hex << c;
-    }
-
-    return srv.publicKey.ApplyFunction(Integer(ss.str().c_str()));
-}
-
-std::string PkcsDecode(const Integer& m)
-{
-    std::stringstream _s;
-    std::string s;
+    if (m.BitCount() != keysize - 14) throw std::invalid_argument("invalid message length");
+    std::vector<char> res;
     uint16_t c;
 
-    _s << std::hex << m;
-    s = _s.str();
-    s.pop_back();
-    if (s.length() <= 2 * 10) throw std::invalid_argument("message length is too short");
-    if (s.length() % 2 == 0) throw std::invalid_argument("message length is even, must be odd");
+    int i;
+    for (i = m.ByteCount() - 2; i >= 0 && m.GetByte(i) != 0; --i)
+        ;
+    if (i < 0) return "";
+    --i;
 
-    int begin = 1;
-    while (begin < s.length() && (s[begin] != '0' || s[begin + 1] != '0'))
-        begin += 2;
-
-    if (begin >= s.length()) return "";
-
-    std::string res((s.length() - begin) / 2, '0');
-    for (int i = begin; i < s.length(); i += 2)
-        res[(i - begin) / 2] = hex2int(s[i + 1]) + 16 * hex2int(s[i]);
-
-    return res;
+    for (; i >= 0; --i)
+        res.push_back(m.GetByte(i));
+    res.push_back(0);
+    return res.data();
 }
 
 // helper class to help messuring times
@@ -163,24 +152,70 @@ class Timer
     }
 };
 
-// helper class to help handling the concept of a set of disjoint intervals
-class Intervals
+// helper class to help predicting process times
+// only used for debbuging, has no actual effect
+class ProgressBar
 {
-    typedef std::pair<Integer, Integer> II;
-    std::set<II> intervals;
+    int num_of_stages;
+    clock_t begin;
 
     public:
-    // returns the intervals themself (as a const set)
-    const std::set<II>& get_intervals() const
+    ProgressBar(int num_of_stages) : num_of_stages(num_of_stages), begin(clock())
     {
-        return intervals;
+    }
+
+    double get_approximation(int stage)
+    {
+        double res = (clock() - begin) / CLOCKS_PER_SEC;
+        res *= num_of_stages;
+        res /= stage;
+        return res;
+    }
+};
+
+class Intervals
+{
+    public:
+    typedef std::pair<Integer, Integer> II;
+    std::vector<II> arr;
+
+    void sort()
+    {
+        std::sort(arr.begin(), arr.end(),
+                  [](const II& i1, const II& i2) { return i1.first > i2.first; });
+
+        int index = 0;
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            if (index != 0 && arr[index - 1].first <= arr[i].second)
+            {
+                while (index != 0 && arr[index - 1].first <= arr[i].second)
+                {
+                    arr[index - 1].second = std::max(arr[index - 1].second, arr[i].second);
+                    arr[index - 1].first = std::min(arr[index - 1].first, arr[i].first);
+                    --index;
+                }
+            }
+            else
+                arr[index] = arr[i];
+
+            ++index;
+        }
+
+        while (arr.size() > index)
+            arr.pop_back();
+    }
+
+    II& front()
+    {
+        return arr.front();
     }
     // returns the sum of sizes of intervals
     // i.e the number of elemnts that belungs to some interval
     Integer size() const
     {
         Integer res = 0;
-        for (const II& p : intervals)
+        for (const II& p : arr)
             res += p.second - p.first + 1;
         return res;
     }
@@ -188,28 +223,13 @@ class Intervals
     // returns the number of disjoint intervals
     int count() const
     {
-        return intervals.size();
+        return arr.size();
     }
 
-    // inserts an interval to the set
-    void insert(II p)
+    void insert(const Integer& a, const Integer& b)
     {
-        auto it = intervals.begin();
-        while (it != intervals.end())
-        {
-            bool not_intersacting = it->first > p.second || it->second < p.first;
-            if (!not_intersacting)
-            {
-                p.first = std::min(p.first, it->first);
-                p.second = std::max(p.second, it->second);
-                intervals.erase(it);
-            }
-            else
-            {
-                it++;
-            }
-        }
-        intervals.insert(p);
+        if (a > b) throw std::runtime_error("algorithmic impossibility");
+        arr.push_back(std::make_pair(a, b));
     }
 };
 
@@ -221,34 +241,51 @@ T div_ceil(const T& n, const T& m)
     return res + 1;
 }
 
+/*
+template <typename T, typename F>
+void map_const(const std::vector<T>& vec, const F& f)
+{
+    dout;
+    for (const T& x : vec)
+        std::cout << std::boolalpha << f(x) << ", ";
+    std::cout << std::endl;
+}
+*/
+
 
 std::string BleichenbacherAttack(Server& srv, const Integer& c)
 {
     // begin attack
     Timer tick("Attack timer", std::cout);
     Integer n = srv.publicKey.GetModulus();
+    ProgressBar pb(n.BitCount());
+
     CryptoPP::ModularArithmetic ma(n);
     Integer B = Integer::Power2(n.BitCount() - 16);
-
-    Integer s = -1;
+    Integer s = 0;
 
     // the current range ogf intervals we search in
     Intervals M;
-    M.insert(std::make_pair(2 * B, 3 * B - 1));
+    M.insert(2 * B, 3 * B - 1);
     // the current interval size
     Integer size = M.size();
     // the best interval size so far
     Integer min_size = size;
 
+
     for (int i = 1; M.size() > 1; ++i)
     {
-        if (i > 1)
-        {
-            size = M.size();
-            if (size.BitCount() < 100) dout << "interval size: " << std::hex << size << std::endl;
-            dout << "size decreased? " << std::boolalpha << (size < min_size) << std::endl;
-            min_size = std::min(min_size, size);
-        }
+        size = M.size();
+        if (size.BitCount() < 100)
+            dout << "interval size = " << size << std::endl;
+        else
+            dout << "intervals size ~ 2^" << size.BitCount() << std::endl;
+
+        double left = pb.get_approximation(n.BitCount() - size.BitCount()) / 3600;
+        if (i != 1) dout << "estimated remainning running time: " << left << " hours" << std::endl;
+        if (size > min_size) throw std::runtime_error("algorithmic impossibility");
+
+        min_size = std::min(min_size, size);
 
         if (i == 1 || M.count() > 1)
         {
@@ -258,16 +295,17 @@ std::string BleichenbacherAttack(Server& srv, const Integer& c)
                 tick.push("step 2.b for i=" + std::to_string(i));
 
             s = (i == 1) ? div_ceil(n, 3 * B) : s + 1;
-            while (!srv.isPkcsConforming(ma.Multiply(srv.publicKey.ApplyFunction(s), c)))
+            while (s < n && !srv.isPkcsConforming(ma.Multiply(srv.publicKey.ApplyFunction(s), c)))
                 s += 1;
 
             tick.pop();
+            if (s >= n) throw std::runtime_error("algorithmic impossibility");
         }
 
         else
         {
             tick.push("step 2.c for i=" + std::to_string(i));
-            Integer a = M.get_intervals().begin()->first, b = M.get_intervals().begin()->second;
+            Integer a = M.front().first, b = M.front().second;
             bool stop_flag = false;
             for (Integer r = div_ceil(2 * b * s - 4 * B, n); !stop_flag; ++r)
             {
@@ -285,29 +323,47 @@ std::string BleichenbacherAttack(Server& srv, const Integer& c)
             tick.pop();
         }
 
+        if (s <= 0 || s >= n) throw std::runtime_error("algorithmic impossibility");
+
         tick.push("step 3 for i=" + std::to_string(i));
         Intervals M_res;
-        for (const auto& p : M.get_intervals())
+        for (const auto& p : M.arr)
         {
             Integer a = p.first, b = p.second;
             Integer begin = div_ceil(a * s - 3 * B + 1, n), end = (b * s - 2 * B) / n;
             for (Integer r = begin; r <= end; ++r)
-                M_res.insert(std::make_pair(std::max(a, div_ceil(2 * B - r * n, s)),
-                                            std::min(b, (3 * B - 1 + r * n) / s)));
+            {
+                Integer na = std::max(a, div_ceil(2 * B + r * n, s)),
+                        nb = std::min(b, (3 * B - 1 + r * n) / s);
+                M_res.insert(na, nb);
+            }
         }
+        M_res.sort();
         M = M_res;
         tick.pop();
     }
 
-    return PkcsDecode(M.get_intervals().begin()->first);
+    dout << "done! interval size = " << M.size() << std::endl;
+
+    return PkcsDecode(M.front().first, srv.keysize);
 }
 
 
 int main(int argc, char* argv[])
 {
     Server srv(2048);
-    Integer c = PkcsEncrypt(srv, "hell0");
-    auto attack_res = BleichenbacherAttack(srv, c);
+    Integer c = srv.PkcsEncrypt("hello world!");
+    std::string attack_res = "wasn't found";
+
+    try
+    {
+        attack_res = BleichenbacherAttack(srv, c);
+    }
+    catch (const std::exception& e)
+    {
+        dout << e.what() << std::endl;
+    }
+
     std::cout << attack_res << std::endl;
 
     return 0;
